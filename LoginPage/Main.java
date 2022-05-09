48a811a592b10bb526655053a6f1d668917b75da
Pho (Tran An) Vu / Haemin <phovu0703@gmail.com>
2022-05-09 03:12:06 -0400

    Add files via upload

---
+++ LoginPage/Main.java
@@ -0,0 +1,9 @@
+package LoginPage;
+
+public class Main {
+    public static void main(String[] args) {
+        // create the object of Login Page to see its working or not
+        new LoginPage();
+
+    }
+}
