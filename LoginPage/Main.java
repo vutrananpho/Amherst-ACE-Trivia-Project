201321e72421bd2d69cd40e647aaca28ff0250a9
Pho (Tran An) Vu / Haemin <phovu0703@gmail.com>
2022-05-14 01:21:38 -0400

    Delete LoginPage directory

--- LoginPage/Main.java
+++
@@ -1,9 +0,0 @@
-package LoginPage;
-
-public class Main {
-    public static void main(String[] args) {
-        // create the object of Login Page to see its working or not
-        new LoginPage();
-
-    }
-}
